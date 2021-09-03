dc7553f6838c8ae324773ee69b1091b9ec1d8242
okkyun <21600437@handong.edu>
2021-09-04 01:37:26 +0900

    second commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		System.out.println("Hello World!!!\n");
 		
 		System.out.print("이름 입력 : ");
 		
@@ -12,6 +12,8 @@ public class Main {
 		String name = sc.next();
 		
 		System.out.printf("[%s]님 환영합니다~\n", name);
+		
+		System.out.println("THis is second commit\n");
 	}
 
 }
